f7142b857d94641862938736fe10cca6661713d4
thatcat13 <thatcat13@gmail.com>
2017-10-20 13:41:00 -0700

    add bootstrap card styling to each response img and text and change text content in html

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,17 @@ $(document).ready(function() {
   var input1 = parseInt($("#creative-logic").val());
   var input2 = parseInt($("#industry").val());
   var input3 = parseInt($("#easy-hard").val());
-  var input4 = parseInt($("#performance").val());
+  var input4 = parseInt($("#start-up").val());
   var input5 = parseInt($("#desktop").val());
 
   var results = score(input1, input2, input3, input4, input5);
 
   if (results <= 6) {
-    $("#front-end, .front-end").show();
+    $("#design, .design, .card").show();
   } else if (results === 7) {
-    $("#full-stack, .full-stack").show();
+    $("#ruby, .ruby, .card").show();
   } else if (results > 7) {
-    $("#back-end, .back-end").show();
+    $("#c-sharp, .c-sharp, .card").show();
   }
 
   });
